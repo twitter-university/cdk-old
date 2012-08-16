e75b3f61521217e1b127bf974b65889a29ff1196
Marko Gargenta <marko@marakana.com>
2012-08-16 14:21:53 -0700

    Added CDK_DIR in shell scripts

--- asc.sh
+++ asc.sh
@@ -2,6 +2,8 @@
 
 [ $# -lt 1 ] && echo "Usage $0 <asciidoc-file> " && exit 1
 
+CDK_DIR=$( cd "$( dirname "$0" )" && pwd )
+
 FILE=$1
 
 attrs=()
@@ -10,5 +12,5 @@ attrs+=("-a deckjs_theme=marakana")
 attrs+=("-a data-uri")
 attrs+=("-a icons")
 
-eval asciidoc ${attrs[@]} $FILE
+eval asciidoc -f $CDK_DIR/external/asciidoc/asciidoc.conf ${attrs[@]} $FILE
 
---
+++ source-highlight.sh
@@ -0,0 +1,4 @@
+# Figure out where CDK is installed
+CDK_DIR=$( cd "$( dirname "$0" )" && pwd )
+
+$CDK/external/source-highlight --data-dir=$CDK_DIR/external/source-highlight-langs
