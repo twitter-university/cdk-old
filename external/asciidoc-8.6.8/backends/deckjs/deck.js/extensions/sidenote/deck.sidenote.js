3a8b3472d134b3faa9e70a511251f20d74fb2870
Marko Gargenta <marko@marakana.com>
2012-08-17 14:49:13 -0700

    Added shell for sidenote deck.js extension

---
+++ external/asciidoc-8.6.8/backends/deckjs/deck.js/extensions/sidenote/deck.sidenote.js
@@ -0,0 +1,16 @@
+/*!
+Deck JS - deck.step
+Copyright (c) 2012 Marakana
+Email marko@marakna.com
+*/
+
+/*
+This module provides a way to hide sidenotes. A sidenote is denoted with **** block 
+in Asciidoc. In HTML by div.sidebarblock.
+*/
+
+(function($, deck, undefined) {
+    var $d = $(document);
+	
+
+})(jQuery, 'deck');
